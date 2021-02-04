eb218c9e4b26d812753af6f8a00d6d54fa301cef
Hannah Joyce Villasin <hvillasin@ucmerced.edu>
2021-02-03 20:50:18 -0800

    unit 2

---
+++ flix/FlixGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  FlixGridCell.swift
+//  flix
+//
+//  Created by Hannah Villasin on 2/3/21.
+//
+
+import UIKit
+
+class FlixGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
